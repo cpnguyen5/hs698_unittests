ac3f2b8846d7d210c7626266b29983da15cb2278
Calvin Nguyen <cpnguyen@calvin-yoga314.(none)>
2016-07-27 18:17:49 -0700

    Omitted fun_things unittests - Travis CI test #2

--- test.py
+++ test.py
@@ -2,28 +2,28 @@ import unittest
 from requester import url_to_csv, batch_url_to_csv, url_to_df
 import numpy as np
 import pandas as pd
-from fun_things import add
+# from fun_things import add
 from numpy.testing import assert_array_almost_equal
 import urllib3
 
-class TestFunThings(unittest.TestCase):
-
-    def test_add(self):
-        res = add(3, 4)
-        self.assertEqual(res, 7)
-
-
-    def test_add_fails_when_input_is_string(self):
-        with self.assertRaises(TypeError):
-            add(3, 'hello')
-
-
-    def test_numpy_array_almost_equal(self):
-        # arr1=np.array([0.0,0.1,0.15])
-        # arr2=np.array([0.0,0.09,0.15])
-        arr1 = np.array([0.0, 0.10000000, 0.15])
-        arr2 = np.array([0.0, 0.10000089, 0.15])
-        assert_array_almost_equal(arr1, arr2, decimal=6)
+# class TestFunThings(unittest.TestCase):
+#
+#     def test_add(self):
+#         res = add(3, 4)
+#         self.assertEqual(res, 7)
+#
+#
+#     def test_add_fails_when_input_is_string(self):
+#         with self.assertRaises(TypeError):
+#             add(3, 'hello')
+#
+#
+#     def test_numpy_array_almost_equal(self):
+#         # arr1=np.array([0.0,0.1,0.15])
+#         # arr2=np.array([0.0,0.09,0.15])
+#         arr1 = np.array([0.0, 0.10000000, 0.15])
+#         arr2 = np.array([0.0, 0.10000089, 0.15])
+#         assert_array_almost_equal(arr1, arr2, decimal=6)
 
 
 class TestRequester(unittest.TestCase):
